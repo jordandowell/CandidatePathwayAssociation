a9b38a441f182df7ac910f9ee4f4e1dfd332c55a
Eric W. Goolsby <eric.goolsby.evolution@gmail.com>
2020-04-11 02:24:58 -0400

    Update functions.R

--- Scripts/functions.R
+++ Scripts/functions.R
@@ -20,8 +20,8 @@ process_data <- function(trait_filename = "katie_data.csv")
   writeLines(text = envs,"environments_to_run.txt")
   
   new_names <- c("SAM",kronecker(X = colnames(dat)[-1],Y = envs,FUN = function(X,Y) paste(X,Y,sep="_")))
-  # dat <- cbind(dat,dat[,-1] + rnorm(n = length(unlist(dat[,-1])),sd = 100),dat[,-1] + rnorm(n = length(unlist(dat[,-1])),sd = 100))
-  dat <- cbind(dat,dat[,-1],dat[,-1])
+  dat <- cbind(dat,dat[,-1]*0 + rnorm(n = length(unlist(dat[,-1]))),dat[,-1]*0 + rnorm(n = length(unlist(dat[,-1]))))
+  #dat <- cbind(dat,dat[,-1],dat[,-1])
   colnames(dat) <- new_names
   
   write.csv(x = dat,file = paste("data/",gsub(pattern = ".csv",replacement = "",x = trait_filename),"_for_pipeline.csv",sep=""),row.names = FALSE)
